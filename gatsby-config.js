16b7ba1c6ee3da56190cd5733ede9809158f443f
Kelly de Vries <kdv@me.com>
2019-04-11 15:12:59 -0700

    add initial files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hookulator`,
+    description: `Hooks explored with a quick calculator app.`,
+    author: `@kdv24`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
